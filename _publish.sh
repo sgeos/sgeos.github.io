b65c23c4a4c35436694227084d09ef4d8d0c3c9c
Brendan A R Sechter <bsechter@sennue.com>
2016-02-23 21:25:17 +0900

    Added script to publish posts.

---
+++ _publish.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "${#}" -lt 1 ]
+then
+  echo "Usage:"
+  echo "  ${0} FILE [FILE...]"
+fi
+
+for FILENAME in "${@}"
+do
+  if [ ! -f ${FILENAME} ]
+  then
+    echo "'${FILENAME}' does not exist."
+  elif [ ! -r ${FILENAME} ] 
+  then
+    echo "'${FILENAME}' can not be read."
+  else
+    DATE=$(sed -n "/^date:[+-:0-9 ]*$/{p; q;}" "${FILENAME}" | awk '{ print $2 }')
+    if [ $DATE ]
+    then
+      echo "mv ${FILENAME} _posts/${DATE}-$(basename $FILENAME)"
+      mv "${FILENAME}" "_posts/${DATE}-$(basename $FILENAME)"
+    else
+      echo "'${FILENAME}' does not have a date."
+    fi
+  fi
+done
+
