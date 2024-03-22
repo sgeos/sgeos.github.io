fbfbe6c663e7f869813d4f0529d4123a126d2d72
Brendan Sechter <sgeos@hotmail.com>
2024-03-22 21:28:35 +0900

    Updated scripts.

--- _new_draft.sh
+++ _new_draft.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env sh # _new_draft.sh
+
 TEMPLATE="_drafts/template.markdown"
 DRAFT="_drafts/${1-new_draft}.markdown"
+
 cp -n "${TEMPLATE}" "${DRAFT}"
 vim "${DRAFT}"
 echo "${DRAFT}"
