a8e55f8964331a5082fcbab309094e6b6613622d
Brendan Sechter <sgeos@hotmail.com>
2025-12-17 11:05:41 -0800

    Safer POSIX-compliant utility scripts.

--- _publish.sh
+++ _publish.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
+# _publish.sh
 
 if [ "${#}" -lt 1 ]
 then
   echo "Usage:"
   echo "  ${0} FILE [FILE...]"
+  exit 1
 fi
 
 for FILENAME in "${@}"
 do
-  if [ ! -f ${FILENAME} ]
+  if [ ! -f "${FILENAME}" ]
   then
     echo "'${FILENAME}' does not exist."
-  elif [ ! -r ${FILENAME} ] 
+  elif [ ! -r "${FILENAME}" ]
   then
-    echo "'${FILENAME}' can not be read."
+    echo "'${FILENAME}' cannot be read."
   else
     DATE=$(sed -n "/^date:[+-:0-9 ]*$/{p; q;}" "${FILENAME}" | awk '{ print $2 }')
-    if [ $DATE ]
+    if [ -n "$DATE" ]
     then
       echo "git add '${FILENAME}'"
       git add "${FILENAME}"
-      echo "mv '${FILENAME}' '_posts/${DATE}-$(basename $FILENAME)'"
-      git mv "${FILENAME}" "_posts/${DATE}-$(basename $FILENAME)"
+      echo "git mv '${FILENAME}' '_posts/${DATE}-$(basename "${FILENAME}")'"
+      git mv "${FILENAME}" "_posts/${DATE}-$(basename "${FILENAME}")"
       #echo "git commit -m 'Published _posts/${DATE}-$(basename $FILENAME).'"
       #git commit -m "Published _posts/${DATE}-$(basename $FILENAME)"
     else
